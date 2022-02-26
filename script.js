42219607393f80fb18602004c2f08f4e36b3b728
Kayla Jones <kayla3445@yahoo.com>
2022-02-26 15:28:38 -0600

    fixed some bugs

--- script.js
+++ script.js
@@ -6,21 +6,12 @@ const pinkDiv = document.querySelector(".pink");
 const pinkLink = document.querySelector(".pink-link");
 const social = document.querySelectorAll(".social-link");
 
+// toggle nav with hamburger click
 hamburger.addEventListener("click", function () {
   nav.classList.toggle("hidden");
 });
 
-const toggleNav = () => {
-  if (window.innerWidth >= 750) {
-    nav.classList.remove("hidden");
-  } else {
-    nav.classList.add("hidden");
-  }
-};
-
-window.addEventListener("resize", toggleNav);
-window.addEventListener("load", toggleNav);
-
+// change color of divs on hover for learn more links
 yellowLink.addEventListener("mouseover", () => {
   yellowDiv.style.backgroundColor = "#fad401";
 });
@@ -37,16 +28,15 @@ pinkLink.addEventListener("mouseout", () => {
   pinkDiv.style.backgroundColor = "#ffdbd4";
 });
 
-social.forEach((el) =>
+// on hover, change imgs for social logos
+social.forEach((el) => {
   el.addEventListener("mouseover", function (e) {
     const platform = e.target.src.slice(34).replace(".svg", "");
     e.target.src = `images/icon-${platform}-active.svg`;
-  })
-);
+  });
 
-social.forEach((el) =>
   el.addEventListener("mouseout", function (e) {
     const platform = e.target.src.slice(34).replace("-active.svg", "");
     e.target.src = `images/icon-${platform}.svg`;
-  })
-);
+  });
+});
