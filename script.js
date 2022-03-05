d9c6e1f04347b2d4d71d94cd50a57f4f202b8489
Kayla Jones <kayla3445@yahoo.com>
2022-03-05 15:37:19 -0600

    test

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const pinkDiv = document.querySelector(".pink");
 const pinkLink = document.querySelector(".pink-link");
 const social = document.querySelectorAll(".social-link");
 const moreInfoLinks = document.querySelectorAll(".more-info-link");
+const links = [yellowLink, pinkLink];
 
 // toggle nav with hamburger click & change image
 hamburger.addEventListener("click", function () {
@@ -17,40 +18,7 @@ hamburger.addEventListener("click", function () {
   }
 });
 
-// change color of divs on hover for learn more links
-const links = [yellowLink, pinkLink];
-links.forEach((el) => {
-  el.addEventListener("mouseover", (e) => {
-    if (e.target.classList.contains("yellow-link")) {
-      yellowDiv.style.backgroundColor = "#fad401";
-    } else if (e.target.classList.contains("pink-link")) {
-      pinkDiv.style.backgroundColor = "#fe7766";
-    }
-  });
-
-  el.addEventListener("mouseout", (e) => {
-    if (e.target.classList.contains("yellow-link")) {
-      yellowDiv.style.backgroundColor = "#fef2b9";
-    } else if (e.target.classList.contains("pink-link")) {
-      pinkDiv.style.backgroundColor = "#ffdbd4";
-    }
-  });
-});
-
-// on hover, change imgs for social logos
-social.forEach((el) => {
-  el.addEventListener("mouseover", function (e) {
-    const platform = e.target.src.slice(34).replace(".svg", "");
-    e.target.src = `images/icon-${platform}-active.svg`;
-  });
-
-  el.addEventListener("mouseout", function (e) {
-    const platform = e.target.src.slice(34).replace("-active.svg", "");
-    e.target.src = `images/icon-${platform}.svg`;
-  });
-});
-
-// touch events for learn more links
+// change color of divs on hover/touch for learn more links
 function addHighlight(e) {
   if (e.target.classList.contains("yellow-link")) {
     yellowDiv.style.backgroundColor = "#fad401";
@@ -74,13 +42,14 @@ links.forEach((el) => {
   el.addEventListener("touchcancel", removeHighlight);
   el.addEventListener("click", addHighlight);
   el.addEventListener("click", removeHighlight);
+  el.addEventListener("mouseover", addHighlight);
+  el.addEventListener("mouseout", removeHighlight);
 });
 
 // more info links touch events
 function moreInfoLinkChange(e) {
   e.target.style.color = "#458d7e";
 }
-
 moreInfoLinks.forEach((el) => {
   el.addEventListener("touchstart", function (e) {
     e.target.style.color = "white";
@@ -90,27 +59,22 @@ moreInfoLinks.forEach((el) => {
   el.addEventListener("touchcancel", moreInfoLinkChange);
 });
 
+// on hover, change imgs for social logos
 function changeSrc(e) {
-  // console.log(e.target.src);
-  // const platform = e.target.src.slice(34).replace(".svg", "");
-  // e.target.src = `images/icon-${platform}-active.svg`;
+  const platform = e.target.src.slice(34).replace(".svg", "");
+  e.target.src = `images/icon-${platform}-active.svg`;
 }
 
-//social imgs touch events
+function resetSrc(e) {
+  const platform = e.target.src.slice(34).replace("-active.svg", "");
+  e.target.src = `images/icon-${platform}.svg`;
+}
+
 social.forEach((el) => {
   el.addEventListener("touchstart", changeSrc);
-
-  // el.addEventListener("touchend", function (e) {
-  //   const platform = e.target.src.slice(34).replace("-active.svg", "");
-  //   e.target.src = `images/icon-${platform}.svg`;
-  // });
-
-  // el.addEventListener("touchmove", function (e) {
-  //   const platform = e.target.src.slice(34).replace("-active.svg", "");
-  //   e.target.src = `images/icon-${platform}.svg`;
-  // });
-  // el.addEventListener("touchcancel", function (e) {
-  //   const platform = e.target.src.slice(34).replace("-active.svg", "");
-  //   e.target.src = `images/icon-${platform}.svg`;
-  // });
+  el.addEventListener("touchend", resetSrc);
+  el.addEventListener("touchmove", resetSrc);
+  el.addEventListener("touchcancel", resetSrc);
+  el.addEventListener("mouseover", changeSrc);
+  el.addEventListener("mouseout", resetSrc);
 });
